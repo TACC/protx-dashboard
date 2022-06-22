d1ab1920cdbe91e1a8e4e1c53d745fc3cebe4fa2
John Gentle <taoteg@gmail.com>
2022-06-22 12:01:26 -0500

    Removed erroneous routing redirect in dashboard./

--- protx-client/src/components/ProTx/components/dashboard/Dashboard.jsx
+++ protx-client/src/components/ProTx/components/dashboard/Dashboard.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useRouteMatch, Redirect } from 'react-router-dom';
 import DashboardDisplay from './DashboardDisplay';
 import '../shared/Variables.css';
 import './Dashboard.css';
-import * as ROUTES from '../../../../constants/routes';
 
 function Dashboard() {
-  const { path } = useRouteMatch();
-  const setupComplete = useSelector(state => state.workbench.setupComplete);
 
   return (
     <div className="dashboard-root">
