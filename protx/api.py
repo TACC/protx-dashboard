c73565c02cd86698a30b4eff7e508f627bf1c690
John Gentle <taoteg@gmail.com>
2022-06-22 17:01:50 -0500

    Added in the login check decorators with the altered m,ethod signature and reorganized a few sections.

--- protx/api.py
+++ protx/api.py
@@ -23,28 +23,35 @@ api = Namespace("api", description="Data related operations", decorators=[onboar
 
 logger = logging.getLogger(__name__)
 
+_DOWNLOAD_FIELDS = ["NAME", "CITY", "STATE", "POSTAL_CODE", "PHONE", "WEBSITE", "LATITUDE", "LONGITUDE", "NAICS_CODE"]
 
+_MALTREATMENT_PLOT = api.model('MaltreatmentPlot', {
+    "area": fields.String(),
+    "selectedArea": fields.String(),
+    "geoid": fields.String(),
+    "variables": fields.String(),
+    "unit": fields.String()
+})
+
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface."""
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+@onboarded_user_required
 @api.route("/analytics")
 class Analytics(Resource):
     @api.doc("get_analytics")
     def get(self):
-        return get_analytics_cached()
-
-
-@memoize_db_results(db_file=analytics.db_name)
-def get_analytics_cached():
-    """Get analytics data"""
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
-    with engine.connect() as connection:
-        # result = connection.execute(DEMOGRAPHICS_QUERY)
-        # data = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS)
-        # result = connection.execute(DEMOGRAPHICS_MIN_MAX_QUERY)
-        # meta = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS[:-1])
-        # return {"data": data, "meta": meta}
+        # return get_analytics_cached()
         return {}
 
 
+@onboarded_user_required
 @api.route("/demographics")
 class Demographics(Resource):
     @api.doc("get_demographics")
@@ -52,19 +59,31 @@ class Demographics(Resource):
         return get_demographics_cached()
 
 
-@memoize_db_results(db_file=demographics.db_name)
-def get_demographics_cached():
-    """Get demographics data"""
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
-    with engine.connect() as connection:
-        result = connection.execute(DEMOGRAPHICS_QUERY)
-        data = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS)
-        result = connection.execute(DEMOGRAPHICS_MIN_MAX_QUERY)
-        meta = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS[:-1])
-        return {"data": data, "meta": meta}
+@onboarded_user_required
+@api.route("/download/<area>/<geoid>/")
+class Resources(Resource):          # Should this be: Download(Resource)
+    @api.doc("get_resources")
+    def get(sel):
+        return get_resources_cached()
 
 
+@onboarded_user_required
+@api.route("/maltreatment")
+class Maltreatment(Resource):
+    @api.doc("get_maltreatment")
+    def get(self):
+        return get_maltreatment_cached()
+
+
+@onboarded_user_required
+@api.route("/resources")
+class Resources(Resource):
+    @api.doc("get_resources")
+    def get(self):
+        return get_resources_cached()
+
+
+@onboarded_user_required
 @api.route("/demographics-plot-distribution/<area>/<geoid>/<variable>/<unit>/")
 class DemographicsDistributionPlotData(Resource):
     @api.doc("get_demographics_distribution_plot_data")
@@ -86,13 +105,13 @@ class DemographicsDistributionPlotData(Resource):
         # return JsonResponse({"result": result})
 
 
+@onboarded_user_required
 @api.route("/display")
 class Display(Resource):
     @api.doc("get_display")
     def get(self):
         """Get display information data"""
         engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
         with engine.connect() as connection:
             display_data = connection.execute("SELECT * FROM display_data")
             result = []
@@ -113,107 +132,10 @@ class Display(Resource):
             return {"variables": result}
 
 
-class Echo:
-    """An object that implements just the write method of the file-like
-    interface.
-    """
-
-    def write(self, value):
-        """Write the value by returning it, instead of storing in a buffer."""
-        return value
-
-
-@api.route("/download/<area>/<geoid>/")
-class Resources(Resource):
-    @api.doc("get_resources")
-    def get(sel):
-        return get_resources_cached()
-
-
-
-_DESIRED_FIELDS = ["NAME", "CITY", "STATE", "POSTAL_CODE", "PHONE", "WEBSITE", "LATITUDE", "LONGITUDE", "NAICS_CODE"]
-
-
-def download_resources(request, area, geoid):
-    """Get display information data
-    """
-    selected_naics_codes = request.GET.getlist("naicsCode")
-
-    if area != "county":
-        # currently assuming county and query is hardcoded for "texas_counties"
-        raise ApiException("Only downloading counties is supported")
-
-    connection = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="protx_geospatial")
-    query = "select * from texas_counties where texas_counties.geo_id='{}'".format(geoid)
-    county_dataframe = geopandas.GeoDataFrame.from_postgis(query, connection, geom_col='geom')
-    county_name = county_dataframe.iloc[0]["name"]
-    connection.close()
-
-    def generate_csv_rows():
-        # header row
-        yield _DESIRED_FIELDS + ["NAICS_DESCRIPTION"]
-
-        resources_result, display_result = get_resources_and_display(naics_codes=selected_naics_codes)
-
-        naics_to_description = {d["NAICS_CODE"]: d["DESCRIPTION"] for d in display_result}
-
-        for r in resources_result:
-            long = r["LONGITUDE"]
-            lat = r["LATITUDE"]
-            if lat and long:  # some resources are missing position
-                point = shapely.geometry.Point(long, lat)
-                if county_dataframe.contains(point).any():
-                    row = [r[key] for key in _DESIRED_FIELDS] + [naics_to_description[r["NAICS_CODE"]]]
-                    yield row
-
-    pseudo_buffer = Echo()
-    writer = csv.writer(pseudo_buffer)
-    response = StreamingHttpResponse(
-        (writer.writerow(row) for row in generate_csv_rows()),
-        content_type="text/csv",
-    )
-
-    # datetime object containing current date and time
-    timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M")
-    filename = f"{county_name}_{area}_resources_{timestamp}.csv"
-    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
-    return response
-
-
-@api.route("/maltreatment")
-class Maltreatment(Resource):
-    @api.doc("get_maltreatment")
-    def get(self):
-        return get_maltreatment_cached()
-
-
-@memoize_db_results(db_file=maltreatment.db_name)
-def get_maltreatment_cached():
-    """Get maltreatment data"""
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
-    with engine.connect() as connection:
-        result = connection.execute(MALTREATMENT_QUERY)
-        data = create_dict(result, level_keys=MALTREATMENT_JSON_STRUCTURE_KEYS)
-        result = connection.execute(MALTREATMENT_MIN_MAX_QUERY)
-        meta = create_dict(result, level_keys=MALTREATMENT_JSON_STRUCTURE_KEYS[:-1])
-        return {"data": data, "meta": meta}
-        # Do we need to wrap the response in Flask?
-        # return JsonResponse({"data": data, "meta": meta})
-
-
-maltreatment_plot = api.model('MaltreatmentPlot', {
-    "area": fields.String(),
-    "selectedArea": fields.String(),
-    "geoid": fields.String(),
-    "variables": fields.String(),
-    "unit": fields.String()
-})
-
-
+@onboarded_user_required
 @api.route("/maltreatment-plot-distribution/")
 class MaltreatmentPlotData(Resource):
-    @api.expect(maltreatment_plot)
+    @api.expect(_MALTREATMENT_PLOT)
     @api.doc("get_maltreatment_plot_data")
     def patch(self):
         """Get maltreatment distribution data for plotting"""
@@ -231,21 +153,93 @@ class MaltreatmentPlotData(Resource):
             unit=unit
         )
         return {"result": result}
-    # Do we need to wrap the response in Flask?
-    # return JsonResponse({"result": result})
+        # Do we need to wrap the response in Flask?
+        # return JsonResponse({"result": result})
 
 
-@api.route("/resources")
-class Resources(Resource):
-    @api.doc("get_resources")
-    def get(self):
-        return get_resources_cached()
+@onboarded_user_required
+def download_resources(request, area, geoid):
+    """Get display information data"""
+    selected_naics_codes = request.GET.getlist("naicsCode")
+
+    if area != "county":
+        # currently assuming county and query is hardcoded for "texas_counties"
+        raise ApiException("Only downloading counties is supported")
+
+    connection = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="protx_geospatial")
+    query = "select * from texas_counties where texas_counties.geo_id='{}'".format(geoid)
+    county_dataframe = geopandas.GeoDataFrame.from_postgis(query, connection, geom_col='geom')
+    county_name = county_dataframe.iloc[0]["name"]
+    connection.close()
+
+    def generate_csv_rows():
+        # header row
+        yield _DOWNLOAD_FIELDS + ["NAICS_DESCRIPTION"]
+
+        resources_result, display_result = get_resources_and_display(naics_codes=selected_naics_codes)
+
+        naics_to_description = {d["NAICS_CODE"]: d["DESCRIPTION"] for d in display_result}
+
+        for r in resources_result:
+            long = r["LONGITUDE"]
+            lat = r["LATITUDE"]
+            if lat and long:  # some resources are missing position
+                point = shapely.geometry.Point(long, lat)
+                if county_dataframe.contains(point).any():
+                    row = [r[key] for key in _DOWNLOAD_FIELDS] + [naics_to_description[r["NAICS_CODE"]]]
+                    yield row
+
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse(
+        (writer.writerow(row) for row in generate_csv_rows()),
+        content_type="text/csv",
+    )
+
+    # datetime object containing current date and time
+    timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M")
+    filename = f"{county_name}_{area}_resources_{timestamp}.csv"
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    return response
 
 
-@memoize_db_results(db_file=resources_db)
-def get_resources_cached():
-    resources_result, display_result = get_resources_and_display()
-    return JsonResponse({"resources": resources_result, "display": display_result})
+# @memoize_db_results(db_file=analytics.db_name)
+# def get_analytics_cached():
+#     """Get analytics data"""
+#     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+#     with engine.connect() as connection:
+#         result = connection.execute(ANALYTICS_QUERY)
+#         data = create_dict(result, level_keys=ANALYTICS_JSON_STRUCTURE_KEYS)
+#         result = connection.execute(ANALYTICS_MIN_MAX_QUERY)
+#         meta = create_dict(result, level_keys=ANALYTICS_JSON_STRUCTURE_KEYS[:-1])
+#         return {"data": data, "meta": meta}
+
+
+@memoize_db_results(db_file=demographics.db_name)
+def get_demographics_cached():
+    """Get demographics data"""
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+    with engine.connect() as connection:
+        result = connection.execute(DEMOGRAPHICS_QUERY)
+        data = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS)
+        result = connection.execute(DEMOGRAPHICS_MIN_MAX_QUERY)
+        meta = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS[:-1])
+        return {"data": data, "meta": meta}
+
+
+@memoize_db_results(db_file=maltreatment.db_name)
+def get_maltreatment_cached():
+    """Get maltreatment data"""
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+    with engine.connect() as connection:
+        result = connection.execute(MALTREATMENT_QUERY)
+        data = create_dict(result, level_keys=MALTREATMENT_JSON_STRUCTURE_KEYS)
+        result = connection.execute(MALTREATMENT_MIN_MAX_QUERY)
+        meta = create_dict(result, level_keys=MALTREATMENT_JSON_STRUCTURE_KEYS[:-1])
+        return {"data": data, "meta": meta}
+        # Do we need to wrap the response in Flask?
+        # return JsonResponse({"data": data, "meta": meta})
+
 
 
 @memoize_db_results(db_file=resources_db)
@@ -262,7 +256,6 @@ def get_resources_and_display(naics_codes=None):
         if naics_codes:
             resource_query += " r WHERE r.NAICS_CODE IN ({})".format(
                 ','.join(['"{}"'.format(code) for code in naics_codes]))
-
         resources = connection.execute(resource_query)
         resources_result = []
         for r in resources:
@@ -272,3 +265,10 @@ def get_resources_and_display(naics_codes=None):
         for m in meta:
             display_result.append(dict(m))
     return resources_result, display_result
+
+
+
+@memoize_db_results(db_file=resources_db)
+def get_resources_cached():
+    resources_result, display_result = get_resources_and_display()
+    return JsonResponse({"resources": resources_result, "display": display_result})
