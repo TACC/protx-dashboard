539a3f34f1fd2dff968732124c23f0ce2a4c2760
John Gentle <taoteg@gmail.com>
2022-06-22 16:07:33 -0500

    Finished the primary mass migration of server-side content for api.

--- protx/api.py
+++ protx/api.py
@@ -28,7 +28,20 @@ logger = logging.getLogger(__name__)
 class Analytics(Resource):
     @api.doc("get_analytics")
     def get(self):
-        # return get_analytics_cached()
+        return get_analytics_cached()
+
+
+@memoize_db_results(db_file=analytics.db_name)
+def get_analytics_cached():
+    """Get analytics data"""
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+    with engine.connect() as connection:
+        # result = connection.execute(DEMOGRAPHICS_QUERY)
+        # data = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS)
+        # result = connection.execute(DEMOGRAPHICS_MIN_MAX_QUERY)
+        # meta = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS[:-1])
+        # return {"data": data, "meta": meta}
         return {}
 
 
@@ -39,7 +52,7 @@ class Demographics(Resource):
         return get_demographics_cached()
 
 
-@memoize_db_results(db_file=cooks_db)
+@memoize_db_results(db_file=demographics.db_name)
 def get_demographics_cached():
     """Get demographics data"""
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
@@ -47,7 +60,6 @@ def get_demographics_cached():
     with engine.connect() as connection:
         result = connection.execute(DEMOGRAPHICS_QUERY)
         data = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS)
-
         result = connection.execute(DEMOGRAPHICS_MIN_MAX_QUERY)
         meta = create_dict(result, level_keys=DEMOGRAPHICS_JSON_STRUCTURE_KEYS[:-1])
         return {"data": data, "meta": meta}
@@ -70,12 +82,15 @@ class DemographicsDistributionPlotData(Resource):
             unit=unit
         )
         return {"result": result}
+        # Do we need to wrap the response in Flask?
+        # return JsonResponse({"result": result})
 
 
 @api.route("/display")
 class Display(Resource):
     @api.doc("get_display")
     def get(self):
+        """Get display information data"""
         engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
         with engine.connect() as connection:
@@ -91,18 +106,88 @@ class Display(Resource):
                     "DISPLAY_MALTREATMENT_RATE"
                 ]:
                     current_value = var[boolean_var_key]
-                    var[boolean_var_key] = True if (current_value == 1 or current_value == "1") else False
+                    var[boolean_var_key] = True if (
+                        current_value == 1 or current_value == "1"
+                    ) else False
                 result.append(var)
             return {"variables": result}
 
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+@api.route("/download/<area>/<geoid>/")
+class Resources(Resource):
+    @api.doc("get_resources")
+    def get(sel):
+        return get_resources_cached()
+
+
+
+_DESIRED_FIELDS = ["NAME", "CITY", "STATE", "POSTAL_CODE", "PHONE", "WEBSITE", "LATITUDE", "LONGITUDE", "NAICS_CODE"]
+
+
+def download_resources(request, area, geoid):
+    """Get display information data
+    """
+    selected_naics_codes = request.GET.getlist("naicsCode")
+
+    if area != "county":
+        # currently assuming county and query is hardcoded for "texas_counties"
+        raise ApiException("Only downloading counties is supported")
+
+    connection = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="protx_geospatial")
+    query = "select * from texas_counties where texas_counties.geo_id='{}'".format(geoid)
+    county_dataframe = geopandas.GeoDataFrame.from_postgis(query, connection, geom_col='geom')
+    county_name = county_dataframe.iloc[0]["name"]
+    connection.close()
+
+    def generate_csv_rows():
+        # header row
+        yield _DESIRED_FIELDS + ["NAICS_DESCRIPTION"]
+
+        resources_result, display_result = get_resources_and_display(naics_codes=selected_naics_codes)
+
+        naics_to_description = {d["NAICS_CODE"]: d["DESCRIPTION"] for d in display_result}
+
+        for r in resources_result:
+            long = r["LONGITUDE"]
+            lat = r["LATITUDE"]
+            if lat and long:  # some resources are missing position
+                point = shapely.geometry.Point(long, lat)
+                if county_dataframe.contains(point).any():
+                    row = [r[key] for key in _DESIRED_FIELDS] + [naics_to_description[r["NAICS_CODE"]]]
+                    yield row
+
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse(
+        (writer.writerow(row) for row in generate_csv_rows()),
+        content_type="text/csv",
+    )
+
+    # datetime object containing current date and time
+    timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M")
+    filename = f"{county_name}_{area}_resources_{timestamp}.csv"
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    return response
+
+
 @api.route("/maltreatment")
 class Maltreatment(Resource):
-    @api.doc("get_matreatment")
+    @api.doc("get_maltreatment")
     def get(self):
         return get_maltreatment_cached()
 
 
-@memoize_db_results(db_file=cooks_db)
+@memoize_db_results(db_file=maltreatment.db_name)
 def get_maltreatment_cached():
     """Get maltreatment data"""
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
@@ -110,10 +195,11 @@ def get_maltreatment_cached():
     with engine.connect() as connection:
         result = connection.execute(MALTREATMENT_QUERY)
         data = create_dict(result, level_keys=MALTREATMENT_JSON_STRUCTURE_KEYS)
-
         result = connection.execute(MALTREATMENT_MIN_MAX_QUERY)
         meta = create_dict(result, level_keys=MALTREATMENT_JSON_STRUCTURE_KEYS[:-1])
         return {"data": data, "meta": meta}
+        # Do we need to wrap the response in Flask?
+        # return JsonResponse({"data": data, "meta": meta})
 
 
 maltreatment_plot = api.model('MaltreatmentPlot', {
@@ -130,12 +216,13 @@ class MaltreatmentPlotData(Resource):
     @api.expect(maltreatment_plot)
     @api.doc("get_maltreatment_plot_data")
     def patch(self):
+        """Get maltreatment distribution data for plotting"""
         area = api.payload["area"]
         selectedArea = api.payload["selectedArea"]
         geoid = api.payload["geoid"]
         variables = api.payload["variables"]
         unit = api.payload["unit"]
-        logger.info("Getting maltreatment plot data for {} {} {} {} on the variables: {}".format(area, selectedArea, unit, geoid, variables))
+        logger.info("Getting maltreatment plot data for {} {} {} {} on the variables: {}".format(area, selectedArea, unit, variables))  # geoid
         result = maltreatment.maltreatment_plot_figure(
             area=area,
             selectedArea=selectedArea,
@@ -144,6 +231,8 @@ class MaltreatmentPlotData(Resource):
             unit=unit
         )
         return {"result": result}
+    # Do we need to wrap the response in Flask?
+    # return JsonResponse({"result": result})
 
 
 @api.route("/resources")
@@ -155,9 +244,26 @@ class Resources(Resource):
 
 @memoize_db_results(db_file=resources_db)
 def get_resources_cached():
+    resources_result, display_result = get_resources_and_display()
+    return JsonResponse({"resources": resources_result, "display": display_result})
+
+
+@memoize_db_results(db_file=resources_db)
+def get_resources_and_display(naics_codes=None):
+    """
+    Get resources and related metadata
+
+    if naics_codes, then limit the resources returned to those
+    that have a NAICS_CODE in naics_codes
+    """
     engine = create_engine(SQLALCHEMY_RESOURCES_DATABASE_URL, connect_args={'check_same_thread': False})
     with engine.connect() as connection:
-        resources = connection.execute("SELECT * FROM business_locations")
+        resource_query = "SELECT * FROM business_locations"
+        if naics_codes:
+            resource_query += " r WHERE r.NAICS_CODE IN ({})".format(
+                ','.join(['"{}"'.format(code) for code in naics_codes]))
+
+        resources = connection.execute(resource_query)
         resources_result = []
         for r in resources:
             resources_result.append(dict(r))
@@ -165,11 +271,4 @@ def get_resources_cached():
         display_result = []
         for m in meta:
             display_result.append(dict(m))
-    return {"resources": resources_result, "display": display_result}
-
-
-@api.route("/download/<area>/<geoid>/")
-class Resources(Resource):
-    @api.doc("get_resources")
-    def get(sel):
-        return get_resources_cached()
+    return resources_result, display_result
