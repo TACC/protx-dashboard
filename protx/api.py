e17e81f3d1db214db3997775fc4da5377e692540
Nathan Franklin <nathan.franklin@gmail.com>
2022-06-22 22:57:03 -0500

    Move code into same spot and revert some formatting
    So its easier to see the diff as lots of diff is due to moving functions
    to different spots

--- protx/api.py
+++ protx/api.py
@@ -28,48 +28,14 @@ api = Namespace("api", description="Data related operations", decorators=[onboar
 
 logger = logging.getLogger(__name__)
 
-_DOWNLOAD_FIELDS = ["NAME", "CITY", "STATE", "POSTAL_CODE", "PHONE", "WEBSITE", "LATITUDE", "LONGITUDE", "NAICS_CODE"]
 
-_MALTREATMENT_PLOT = api.model('MaltreatmentPlot', {
+maltreatment_plot = api.model('MaltreatmentPlot', {
     "area": fields.String(),
     "selectedArea": fields.String(),
     "geoid": fields.String(),
     "variables": fields.String(),
     "unit": fields.String()
 })
-
-
-class Echo:
-    """An object that implements just the write method of the file-like
-    interface."""
-    def write(self, value):
-        """Write the value by returning it, instead of storing in a buffer."""
-        return value
-
-
-@onboarded_user_required
-@api.route("/analytics")
-class Analytics(Resource):
-    @api.doc("get_analytics")
-    def get(self):
-        # return get_analytics_cached()
-        return {}
-
-
-@onboarded_user_required
-@api.route("/demographics")
-class Demographics(Resource):
-    @api.doc("get_demographics")
-    def get(self):
-        return get_demographics_cached()
-
-
-@onboarded_user_required
-@api.route("/download/<area>/<geoid>/")
-class Resources(Resource):          # Should this be: Download(Resource)
-    @api.doc("get_resources")
-    def get(sel):
-        return get_resources_cached()
 
 
 @onboarded_user_required
@@ -81,10 +47,27 @@ class Maltreatment(Resource):
 
 
 @onboarded_user_required
+@api.route("/demographics")
+class Demographics(Resource):
+    @api.doc("get_demographics")
+    def get(self):
+        return get_demographics_cached()
+
+
+@onboarded_user_required
 @api.route("/resources")
 class Resources(Resource):
     @api.doc("get_resources")
     def get(self):
+        return get_resources_cached()
+
+
+@onboarded_user_required
+@api.route("/download/<area>/<geoid>/")
+class DownloadResources(Resource):
+    @api.doc("get_resources")
+    def get(self):
+        # TODO
         return get_resources_cached()
 
 
@@ -109,36 +92,9 @@ class DemographicsDistributionPlotData(Resource):
 
 
 @onboarded_user_required
-@api.route("/display")
-class Display(Resource):
-    @api.doc("get_display")
-    def get(self):
-        """Get display information data"""
-        engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-        with engine.connect() as connection:
-            display_data = connection.execute("SELECT * FROM display_data")
-            result = []
-            for variable_info in display_data:
-                var = dict(variable_info)
-                # Interpret some variables used to control dropdown population https://jira.tacc.utexas.edu/browse/COOKS-148
-                for boolean_var_key in [
-                    "DISPLAY_DEMOGRAPHIC_COUNT",
-                    "DISPLAY_DEMOGRAPHIC_RATE",
-                    "DISPLAY_MALTREATMENT_COUNT",
-                    "DISPLAY_MALTREATMENT_RATE"
-                ]:
-                    current_value = var[boolean_var_key]
-                    var[boolean_var_key] = True if (
-                        current_value == 1 or current_value == "1"
-                    ) else False
-                result.append(var)
-            return {"variables": result}
-
-
-@onboarded_user_required
 @api.route("/maltreatment-plot-distribution/")
 class MaltreatmentPlotData(Resource):
-    @api.expect(_MALTREATMENT_PLOT)
+    @api.expect(maltreatment_plot)
     @api.doc("get_maltreatment_plot_data")
     def patch(self):
         """Get maltreatment distribution data for plotting"""
@@ -159,61 +115,24 @@ class MaltreatmentPlotData(Resource):
 
 
 @onboarded_user_required
-def download_resources(request, area, geoid):
-    """Get display information data"""
-    selected_naics_codes = request.GET.getlist("naicsCode")
-
-    if area != "county":
-        # currently assuming county and query is hardcoded for "texas_counties"
-        abort(500, "Only downloading counties is supported")
-
-    connection = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="protx_geospatial")
-    query = "select * from texas_counties where texas_counties.geo_id='{}'".format(geoid)
-    county_dataframe = geopandas.GeoDataFrame.from_postgis(query, connection, geom_col='geom')
-    county_name = county_dataframe.iloc[0]["name"]
-    connection.close()
-
-    def generate_csv_rows():
-        # header row
-        yield _DOWNLOAD_FIELDS + ["NAICS_DESCRIPTION"]
-
-        resources_result, display_result = get_resources_and_display(naics_codes=selected_naics_codes)
-
-        naics_to_description = {d["NAICS_CODE"]: d["DESCRIPTION"] for d in display_result}
-
-        for r in resources_result:
-            long = r["LONGITUDE"]
-            lat = r["LATITUDE"]
-            if lat and long:  # some resources are missing position
-                point = shapely.geometry.Point(long, lat)
-                if county_dataframe.contains(point).any():
-                    row = [r[key] for key in _DOWNLOAD_FIELDS] + [naics_to_description[r["NAICS_CODE"]]]
-                    yield row
-
-    pseudo_buffer = Echo()
-    writer = csv.writer(pseudo_buffer)
-    response = StreamingHttpResponse(
-        (writer.writerow(row) for row in generate_csv_rows()),
-        content_type="text/csv",
-    )
-
-    # datetime object containing current date and time
-    timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M")
-    filename = f"{county_name}_{area}_resources_{timestamp}.csv"
-    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
-    return response
-
-
-# @memoize_db_results(db_file=analytics.db_name)
-# def get_analytics_cached():
-#     """Get analytics data"""
-#     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-#     with engine.connect() as connection:
-#         result = connection.execute(ANALYTICS_QUERY)
-#         data = create_dict(result, level_keys=ANALYTICS_JSON_STRUCTURE_KEYS)
-#         result = connection.execute(ANALYTICS_MIN_MAX_QUERY)
-#         meta = create_dict(result, level_keys=ANALYTICS_JSON_STRUCTURE_KEYS[:-1])
-#         return {"data": data, "meta": meta}
+@api.route("/display")
+class Display(Resource):
+    @api.doc("get_display")
+    def get(self):
+        """Get display information data"""
+        engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+        with engine.connect() as connection:
+            display_data = connection.execute("SELECT * FROM display_data")
+            result = []
+            for variable_info in display_data:
+                var = dict(variable_info)
+                # Interpret some variables used to control dropdown population https://jira.tacc.utexas.edu/browse/COOKS-148
+                for boolean_var_key in ["DISPLAY_DEMOGRAPHIC_COUNT", "DISPLAY_DEMOGRAPHIC_RATE",
+                                        "DISPLAY_MALTREATMENT_COUNT", "DISPLAY_MALTREATMENT_RATE"]:
+                    current_value = var[boolean_var_key]
+                    var[boolean_var_key] = True if (current_value == 1 or current_value == "1") else False
+                result.append(var)
+            return {"variables": result}
 
 
 @memoize_db_results(db_file=demographics.db_name)
@@ -269,3 +188,59 @@ def get_resources_and_display(naics_codes=None):
 def get_resources_cached():
     resources_result, display_result = get_resources_and_display()
     return {"resources": resources_result, "display": display_result}
+
+_DOWNLOAD_FIELDS = ["NAME", "CITY", "STATE", "POSTAL_CODE", "PHONE", "WEBSITE", "LATITUDE", "LONGITUDE", "NAICS_CODE"]
+
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface."""
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+@onboarded_user_required
+def download_resources(request, area, geoid):
+    """Get display information data"""
+    selected_naics_codes = request.GET.getlist("naicsCode")
+
+    if area != "county":
+        # currently assuming county and query is hardcoded for "texas_counties"
+        abort(500, "Only downloading counties is supported")
+
+    connection = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="protx_geospatial")
+    query = "select * from texas_counties where texas_counties.geo_id='{}'".format(geoid)
+    county_dataframe = geopandas.GeoDataFrame.from_postgis(query, connection, geom_col='geom')
+    county_name = county_dataframe.iloc[0]["name"]
+    connection.close()
+
+    def generate_csv_rows():
+        # header row
+        yield _DOWNLOAD_FIELDS + ["NAICS_DESCRIPTION"]
+
+        resources_result, display_result = get_resources_and_display(naics_codes=selected_naics_codes)
+
+        naics_to_description = {d["NAICS_CODE"]: d["DESCRIPTION"] for d in display_result}
+
+        for r in resources_result:
+            long = r["LONGITUDE"]
+            lat = r["LATITUDE"]
+            if lat and long:  # some resources are missing position
+                point = shapely.geometry.Point(long, lat)
+                if county_dataframe.contains(point).any():
+                    row = [r[key] for key in _DOWNLOAD_FIELDS] + [naics_to_description[r["NAICS_CODE"]]]
+                    yield row
+
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse(
+        (writer.writerow(row) for row in generate_csv_rows()),
+        content_type="text/csv",
+    )
+
+    # datetime object containing current date and time
+    timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M")
+    filename = f"{county_name}_{area}_resources_{timestamp}.csv"
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    return response
