9c4ae7ac27a0d47e4917a9da76636981e525f112
Nathan Franklin <nathan.franklin@gmail.com>
2022-06-30 11:12:55 -0500

    task/COOKS-246: configure client app for deployment (#7)
    * Build client in docker image
    
    * Remove unused file
    
    * Allow running of built spa (for local testing)
    
    * Revert "Remove unused file"
    
    This reverts commit a72cb72e5f4864ada172d3e8216fe286d0268b04.
    
    * Configure so built client can be used/tested locally
    
    USE_DEV_CLIENT is set to true for serving client via "npm run dev"
    
    * Fix template path
    
    Co-authored-by: John Gentle <taoteg@gmail.com>

--- protx/app.py
+++ protx/app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 from flask_restx import Api
 from protx.api import api as protx_api
 import logging
+import os
+
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 app = Flask(__name__)
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+use_dev_client = os.getenv('USE_DEV_CLIENT') == 'true'
+app.template_folder = "templates" if use_dev_client else "../protx-client/dist/"
+template = "dash.html" if use_dev_client else "index.html"
 
 api = Api(app, title="ProTx API", version="1.0", description="Protx")
 
@@ -14,8 +20,4 @@ api.add_namespace(protx_api, path="/protx/api")
 
 @app.route("/protx/dash/<path:path>")
 def index(path=None):
-    # TODO: Render alternate template for the compiled bundle.
-    # https://jira.tacc.utexas.edu/browse/COOKS-256
-    return render_template("dash.html")
-
-# TODO settings
+    return render_template(template)
