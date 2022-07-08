78907ed91499bb4a6f9910dfe8ca9dcb6843255b
Nathan Franklin <nathan.franklin@gmail.com>
2022-07-08 14:40:18 -0500

    Task/COOKS-282: fix access from protx to core (#15)
    * Update setupComplete logic as CEP Core reponse has changed
    
    Previously, /api/workbench required authed users and always had a
    setupComplete value.  Now guests can request it and then there is no
    setupComplete value.
    
    This changes where a redirect to login would go so moving [COOKS-279]
    comments to that spot.
    
    * Use host instead of service when getting /workbench/onboarding info on prod/staging
    
    * Fix linting

--- protx/decorators.py
+++ protx/decorators.py
@@ -11,14 +11,31 @@ logger = logging.getLogger(__name__)
 cache = Cache("database_cache")
 
 
+def get_host():
+    host = request.url_root.split(',')[0] if ',' in request.url_root else request.url_root
+    return host
+
+
 def is_setup_complete() -> bool:
-    # making request directly to core django. alternatively, we could
-    # make the request to the request.get_host() (but then a bit
-    # tricky if it is local development and accessing cep.dev)
-    r = requests.get("http://core:6000/api/workbench/", cookies=request.cookies)
+    host = get_host()
+    if host.endswith("cep.dev"):
+        # unable to access cep.dev (as actual site on web) so using docker service instead.  Note that staging/prod are
+        # unable to use the service directly (https-requirement for uwsgi configs?)
+        host = "http://core:6000/"
+    r = requests.get(host + "api/workbench/", cookies=request.cookies)
     r.raise_for_status()
     json_response = r.json()
-    return json_response['response']['setupComplete']
+    if 'setupComplete' in json_response['response']:
+        return json_response['response']['setupComplete']
+    else:
+        # User isn't logged in which is unexpected as pages (which contain this SPA as an iframe)
+        # are controlled by CMS which should require a login
+        #
+        # If if the future, CMS are no longer configured to keep pages behind login, then we should refactor
+        # this function and `onboarded_user_setup_complete` to redirect to login instead of /workbench/onboarding if
+        # user isn't logged in.  See https://jira.tacc.utexas.edu/browse/COOKS-279
+        logger.error("User not logged in which is unexpected as CMS should be blocking pages")
+        return False
 
 
 def onboarded_user_setup_complete(function):
@@ -28,10 +45,8 @@ def onboarded_user_setup_complete(function):
     def wrapper(*args, **kwargs):
         try:
             if not is_setup_complete():
-                redirect_url = request.url_root.split(',')[0] if ',' in request.url_root else request.url_root
-                return redirect(redirect_url + '/workbench/onboarding')
+                return redirect(get_host() + '/workbench/onboarding')
         except Exception as e:
-            # User isn't logged in which is unexpected as pages (which contain this SPA as an iframe) are controlled by CMS which should require a login
             logger.error(e)
             raise Forbidden
         else:
