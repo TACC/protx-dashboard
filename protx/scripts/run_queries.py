674689e2c5bc815c62832a4a508c16d0988ca5bb
Nathan Franklin <nathan.franklin@gmail.com>
2022-07-14 21:32:06 -0500

    Improve print statements

--- protx/scripts/run_queries.py
+++ protx/scripts/run_queries.py
@@ -3,12 +3,16 @@ import time
 
 def call_function_and_report(f):
     start = time.time()
-    print(f"{f.__name__} is running")
+    print(f"{f.__name__} ...")
     f()
     total_time = time.time() - start
     print(f"  {f.__name__} completed in {total_time} seconds")
 
 functions_to_cache = [get_maltreatment_cached, get_demographics_cached, get_resources_cached]
 
+print(f" Running {len(functions_to_cache)} methods in order to cache their results")
+
 for func in functions_to_cache:
     call_function_and_report(func)
+
+print("Completed.")
