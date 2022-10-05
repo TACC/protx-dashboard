1d0e73d5f7a43a0bac88b2040c0f3e305c2a79f3
Nathan Franklin <nathan.franklin@gmail.com>
2022-10-05 15:49:29 -0500

    Add border to dfps regions

--- protx-client/src/components/ProTx/components/shared/mapUtils.js
+++ protx-client/src/components/ProTx/components/shared/mapUtils.js
@@ -30,6 +30,9 @@ const getFeatureStyle = (
   maltreatmentTypes,
   unit
 ) => {
+  const stroke = geography == 'dfps_region' ? true : false;
+  const color = 'black';
+  const weight = 1;
   let fillColor;
   if (mapType === 'observedFeatures') {
     const featureValue = getObservedFeatureValue(
@@ -60,16 +63,19 @@ const getFeatureStyle = (
     return {
       fillColor,
       fill: true,
-      stroke: false,
-      fillOpacity: 0.5
+      fillOpacity: 0.5,
+      stroke,
+      color,
+      weight,
     };
   }
-  // if no color/data, we return a completely transparent style in order
-  // to allow for feature selection.
+  // if no color/data, we return a transparent style in order to allow for feature selection.
   return {
     fillColor: 'black',
     fill: true,
-    stroke: false,
+    stroke,
+    color,
+    weight,
     fillOpacity: 0.0
   };
 };
