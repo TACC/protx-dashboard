039f9f45530fbdce44d85a263c6dd784cdae85f2
ryamase <ryamase@gmail.com>
2022-10-21 12:06:52 -0500

    add caption text

--- protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
+++ protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { LoadingSpinner } from '_common';
 import MainPlot from './MainPlot';
 import { FigureCaption } from './FigureCaption';
+import {getSelectedGeographyName} from "../shared/dataUtils";
 
 
 
@@ -15,7 +16,10 @@ function AnalyticsStateDistribution({geography, selectedGeographicFeature}) {
   );
   
   const plotLabel = 'Figure 1.';
-  const plotCaptionText = `Distribution of projected number of cases across counties in Texas. 
+  const plotCaptionText = selectedGeographicFeature ? 
+  [`Distribution of projected number of cases across counties in Texas. Black vertical lines indicate thresholds used to define high, 
+  medium and low risk regions for heat map on the left. The red vertical line indicates where `, `${getSelectedGeographyName(selectedGeographicFeature)}`, ` County falls on this distribution.`] 
+  : `Distribution of projected number of cases across counties in Texas. 
   Black vertical lines indicate thresholds used to define high, medium and low risk regions for heat map 
   on the left.`;
 
@@ -47,7 +51,10 @@ function AnalyticsStateDistribution({geography, selectedGeographicFeature}) {
   }
 
   return (
-    <MainPlot plotState={chartData.data} />
+    <>
+      <MainPlot plotState={chartData.data} />
+      <FigureCaption label={plotLabel} captionText={plotCaptionText} />
+    </>
   );
 }
 
