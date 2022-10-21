9a1c3cd08d2d0d3581164a05210aa3f30191a5ac
Nathan Franklin <nathan.franklin@gmail.com>
2022-10-20 23:49:08 -0500

    Update tests

--- protx/tests/api_test.py
+++ protx/tests/api_test.py
@@ -186,7 +186,7 @@ def test_get_analytics_for_specific_area_setup_complete_false(test_client, core_
 
 @pytest.mark.skipif(missing_database_directory(), reason="requires database directory or to-be-done database fixtures")
 def test_get_analytics_chart(test_client, core_api_workbench_request):
-    resp = test_client.get('/protx/api/analytics-chart/county/risk/')
+    resp = test_client.get('/protx/api/analytics-chart/county/risk')
     assert resp.status_code == 200
     data = resp.get_json()
     assert "result" in data
@@ -194,10 +194,10 @@ def test_get_analytics_chart(test_client, core_api_workbench_request):
 
 
 def test_get_analytics_chart_unauthed(test_client, core_api_workbench_request_unauthed):
-    resp = test_client.get('/protx/api/analytics-chart/county/risk/')
+    resp = test_client.get('/protx/api/analytics-chart/county/risk')
     assert resp.status_code == 403
 
 
 def test_get_analytics_chart_complete_false(test_client, core_api_workbench_request_setup_complete_false):
-    resp = test_client.get('/protx/api/analytics-chart/county/risk/')
+    resp = test_client.get('/protx/api/analytics-chart/county/risk')
     assert resp.status_code == 403
