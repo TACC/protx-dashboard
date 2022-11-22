89c2e92a59ed93d5b8e719fdbe31267d500e8552
Nathan Franklin <nathan.franklin@gmail.com>
2022-11-22 17:06:38 -0600

    Add unit tests

--- protx/tests/app_test.py
+++ protx/tests/app_test.py
@@ -1,3 +1,19 @@
+def test_dash_analytics(test_client, core_api_workbench_request):
+    response = test_client.get('/protx/dash/analytics')
+    assert response.status_code == 200
+
+
+def test_dash_analytics_redirect_with_setup_complete_false(test_client, core_api_workbench_request_setup_complete_false):
+    response = test_client.get('/protx/dash/analytics')
+    assert response.status_code == 302
+    assert response.location == 'http://localhost//protx/onboarding'
+
+
+def test_dash_onboarding(test_client, core_api_workbench_request):
+    response = test_client.get('/protx/onboarding')
+    assert response.status_code == 200
+
+
 def test_verify_user_redirect_with_setup_complete_false(test_client, core_api_workbench_request_setup_complete_false):
     response = test_client.get('/protx/verify-user')
     assert response.status_code == 302
