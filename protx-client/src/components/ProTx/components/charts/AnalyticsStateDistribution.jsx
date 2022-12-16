ede5e5e74dbcee69595b2cff0ad6327e3798a7aa
ryamase <ryamase@gmail.com>
2022-12-16 08:49:31 -0600

    refactor FigureCaption component

--- protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
+++ protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
@@ -19,21 +19,21 @@ function AnalyticsStateDistribution({geography, selectedGeographicFeature}) {
   const analytics = useSelector(
     state => state.protxAnalytics
   );
-  
+
   let countyName;
   if (selectedGeographicFeature) {
     countyName = getSelectedGeographyName(geography, selectedGeographicFeature)
   }
 
   useEffect(() => {
-      dispatch({
-        type: 'FETCH_PROTX_ANALYTICS_STATEWIDE_DISTRIBUTION',
-        payload: {
-          area: geography,
-          analyticsType: 'pred_per_100k',
+    dispatch({
+      type: 'FETCH_PROTX_ANALYTICS_STATEWIDE_DISTRIBUTION',
+      payload: {
+        area: geography,
+        analyticsType: 'pred_per_100k',
           geoid: selectedGeographicFeature
         }
-      });
+    });
   }, [geography, selectedGeographicFeature]);
 
   if (chartData.error || analytics.error) {
@@ -52,36 +52,53 @@ function AnalyticsStateDistribution({geography, selectedGeographicFeature}) {
     );
   }
 
-  const plotLabel = 'Figure 1.';
-  let plotCaptionText = [`Distribution of projected number of cases across counties in Texas. 
-  Black vertical lines indicate thresholds used to define high, 
-  medium and low risk regions for heat map on the left.`]
+  let conditionalCaptionJSX;
 
   if (selectedGeographicFeature) {
     if (analytics.data.pred_per_100k) {
-        plotCaptionText.push([`The red vertical line indicates 
-        where `, <span className='annotation-text-bold'>{countyName} County</span>, ` falls on this distribution.`]);
+      conditionalCaptionJSX = (
+        <>
+          {' '}
+          The red vertical line indicates where{' '}
+          <span className='annotation-text-bold'>{countyName} County</span>{' '}
+          falls on this distribution.
+        </>
+      );
     } else {
-      plotCaptionText.push([
-        <span className='annotation-text-bold'> Note: There is no data for {countyName} County.</span>,
-      ]);
+      conditionalCaptionJSX = (
+        <span className='annotation-text-bold'>
+          {' '}
+          Note: There is no data for {countyName} County.
+        </span>
+      );
     }
   }
+
+  const plotCaptionJSX = (
+    <>
+      Distribution of projected number of cases across counties in Texas. Black
+      vertical lines indicate thresholds used to define high, medium and low
+      risk regions for heat map on the left.
+      {conditionalCaptionJSX}
+    </>
+  );
 
   return (
     <div class="maltreatment-types-plot-layout">
       <div className="feature-table">
           <div className="feature-table-chart-selection">
             <div className="feature-table-chart-title">
-              Definition of Risk Levels
+            Definition of Risk Levels
               <span className="feature-table-chart-subtitle">
               (Figure 1)
               </span>
-            </div>
           </div>
         </div>
+      </div>
       <MainPlot plotState={chartData.data} />
-      <FigureCaption label={plotLabel} captionText={plotCaptionText} />
+      <FigureCaption label={'Figure 1.'} className={'chart-annotation'}>
+        {plotCaptionJSX}
+      </FigureCaption>
     </div>
   );
 }
