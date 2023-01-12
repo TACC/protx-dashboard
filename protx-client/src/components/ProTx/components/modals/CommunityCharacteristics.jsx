ce2da5d79dc0cb2dc94efa7c5ad72c28513061c1
Nathan Franklin <nathan.franklin@gmail.com>
2023-01-11 21:31:37 -0600

    Add dialog for community characteristics

---
+++ protx-client/src/components/ProTx/components/modals/CommunityCharacteristics.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import { useQuery } from 'react-query';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import PropTypes from 'prop-types';
+import styles from './CommunityCharacteristics.module.scss';
+import { LoadingSpinner } from '_common/index';
+import Cookies from 'js-cookie';
+import MainPlot from '../charts/MainPlot';
+
+const getData = async (geography, geoid) => {
+  const url = `/protx/api/demographics-community-characteristics-chart/${geography}/${geoid}/`;
+  const request = await fetch(url, {
+    headers: { 'X-CSRFToken': Cookies.get('csrftoken') },
+    credentials: 'same-origin',
+  });
+  return request.json();
+};
+
+const CommunityCharacteristicsChart = ({
+  geography,
+  selectedGeographicFeature,
+}) => {
+  const { data, isFetching, isError } = useQuery(
+    ['community-characteristics', geography, selectedGeographicFeature],
+    () => getData(geography, selectedGeographicFeature)
+  );
+
+  if (isFetching) {
+    return (
+      <div className={styles.loading}>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (isError) {
+    debugger;
+    return <SectionMessage type="error">something went wrong</SectionMessage>;
+  }
+
+  debugger;
+  return <MainPlot plotState={data.result} />;
+};
+CommunityCharacteristicsChart.propTypes = {
+  selectedGeographicFeature: PropTypes.string.isRequired,
+  geographyLabel: PropTypes.string.isRequired,
+};
+
+const CommunityCharacteristics = ({
+  isOpen,
+  toggle,
+  geography,
+  selectedGeographicFeature,
+  geographyLabel,
+}) => {
+  return (
+    <Modal size="xl" isOpen={isOpen} toggle={toggle}>
+      <ModalHeader toggle={toggle} charCode="&#xe912;">
+        Community Characteristics for {geographyLabel}
+      </ModalHeader>
+      <ModalBody>
+        <CommunityCharacteristicsChart
+          geography={geography}
+          selectedGeographicFeature={selectedGeographicFeature}
+        />
+      </ModalBody>
+    </Modal>
+  );
+};
+
+CommunityCharacteristics.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
+  geography: PropTypes.string.isRequired,
+  selectedGeographicFeature: PropTypes.string.isRequired,
+  geographyLabel: PropTypes.string.isRequired,
+};
+
+export default CommunityCharacteristics;
--- protx-client/src/main.tsx
+++ protx-client/src/main.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import LoadingSpinner from '_common/LoadingSpinner';
 import App from './App';
 import './index.css';
 import store from './redux/store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <Provider store={store}>
+        <Suspense fallback={<LoadingSpinner />}>
+          <App />
+        </Suspense>
+      </Provider>
+    </React.StrictMode>
+  </QueryClientProvider>,
   document.getElementById('root')
 );
