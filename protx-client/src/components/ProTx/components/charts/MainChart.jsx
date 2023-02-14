9e488988e9b550f6bb78820b510f937865a6731f
Nathan Franklin <nathan.franklin@gmail.com>
2023-02-14 10:37:08 -0600

    Hotifx/redirect and demographics chart height (#89)
    * Update redirect
    
    * Set max height for demographics chart
    
    Was too larger with extra space
    
    * Add step to README
    
    * Fix type issue
    
    Unclear where 'demographics' is coming from; should be
    'observedFeatures'
    
    * Make CommunityCharacteristic a little shorter
    
    * Refactor how we et max height for demographics chart to use SASS
    
    * Fix redirect
    
    * Bring back code
    
    * Fix prettier issues
    
    * Patch/merge prep/hotifx/redirect and demographics chart height (#90)
    
    * Bump d3-color and plotly.js in /protx-client (#88)
    
    Bumps [d3-color](https://github.com/d3/d3-color) and [plotly.js](https://github.com/plotly/plotly.js). These dependencies needed to be updated together.
    
    Updates `d3-color` from 1.4.1 to 3.1.0
    - [Release notes](https://github.com/d3/d3-color/releases)
    - [Commits](https://github.com/d3/d3-color/compare/v1.4.1...v3.1.0)
    
    Updates `plotly.js` from 2.18.0 to 2.18.1
    - [Release notes](https://github.com/plotly/plotly.js/releases)
    - [Changelog](https://github.com/plotly/plotly.js/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/plotly/plotly.js/compare/v2.18.0...v2.18.1)
    
    ---
    updated-dependencies:
    - dependency-name: d3-color
      dependency-type: indirect
    - dependency-name: plotly.js
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Cleaned up the scss and css redundancy, also added in the dependabot bumps so this is ready top merge into main cleanly.
    
    * Removing unused lines
    
    * Using style/scss for loading/error messages
    
    * Use SASS modules
    
    * Adjust spinner and error message margins
    
    * Remove unneeded loading spinner class
    
    * Fix linting errors
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: John Gentle <taoteg@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- protx-client/src/components/ProTx/components/charts/MainChart.jsx
+++ protx-client/src/components/ProTx/components/charts/MainChart.jsx
@@ -8,7 +8,7 @@ import AnalyticsStateDistribution from './AnalyticsStateDistribution';
 import DemographicsDetails from './DemographicsDetails';
 import MaltreatmentDetails from './MaltreatmentDetails';
 import MainPlot from './MainPlot';
-import './MainChart.css';
+import styles from './MainChart.module.scss';
 import { getSelectedGeographyName } from '../shared/dataUtils';
 
 function MainChart({ data, showInstructions }) {
@@ -112,18 +112,14 @@ function MainChart({ data, showInstructions }) {
     if (selection.selectedGeographicFeature && selection.observedFeature) {
       if (protxDemographicsDistribution.error) {
         return (
-          <div className="data-error-message">
+          <div className={styles['data-error-message']}>
             There was a problem loading the data.
           </div>
         );
       }
 
       if (protxDemographicsDistribution.loading) {
-        return (
-          <div className="loading-spinner">
-            <LoadingSpinner />
-          </div>
-        );
+        return <LoadingSpinner />;
       }
 
       const plotState = protxDemographicsDistribution.data;
@@ -136,7 +132,10 @@ function MainChart({ data, showInstructions }) {
             selectedGeographicFeature={selection.selectedGeographicFeature}
             data={data}
           />
-          <MainPlot plotState={plotState} />
+          <MainPlot
+            plotState={plotState}
+            className={styles['demographics-plot']}
+          />
           {!protxDemographicsDistribution.loading && (
             <ChartInstructions currentReportType="hidden" />
           )}
@@ -153,18 +152,14 @@ function MainChart({ data, showInstructions }) {
     ) {
       if (protxMaltreatmentDistribution.error) {
         return (
-          <div className="data-error-message">
+          <div className={styles['data-error-message']}>
             There was a problem loading the data.
           </div>
         );
       }
 
       if (protxMaltreatmentDistribution.loading) {
-        return (
-          <div className="loading-spinner">
-            <LoadingSpinner />
-          </div>
-        );
+        return <LoadingSpinner />;
       }
 
       const plotState = protxMaltreatmentDistribution.data;
