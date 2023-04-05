ce03e027ad21185788607bbdb3e54575327a6a71
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-04-05 15:28:04 -0500

    Update protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>

--- protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
+++ protx-client/src/components/ProTx/components/charts/AnalyticsStateDistribution.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { LoadingSpinner } from '_common';
