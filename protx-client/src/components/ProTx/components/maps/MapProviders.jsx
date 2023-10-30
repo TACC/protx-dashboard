d60642b0a66c958fca893117c0a94f1db5eb075b
taoteg <taoteg@gmail.com>
2023-10-30 14:38:13 -0500

    Updated the tiles provider source array with working tile serviuces from the leaflet providers preview page.

--- protx-client/src/components/ProTx/components/maps/MapProviders.jsx
+++ protx-client/src/components/ProTx/components/maps/MapProviders.jsx
@@ -4,66 +4,54 @@ export default function Maptiles() {
   const providers = [];
   const layers = {};
 
-  // Provider: Open Street Maps
-  // No key required.
-
   // eslint-disable-next-line no-unused-vars
-  const basemapOsmDefault = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  const tiles_CartoDBPositron = L.tileLayer('https://abcd.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     {
-      attribution:
-        'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       minZoom: 0,
-      maxZoom: 17,
-      name: 'OSM - Default',
+      maxZoom: 20,
+      name: 'CartoDB Positron',
       type: 'png',
     }
   );
 
   // eslint-disable-next-line no-unused-vars
-  const basemapOsmBw = L.tileLayer(
-    'https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
+  const tiles_StadiaAlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
     {
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
-      maxZoom: 18,
-      name: 'OSM - Mapnik B&W',
+      maxZoom: 20,
+      name: 'Stadia - Alidade Smooth',
       type: 'png',
     }
   );
 
   // eslint-disable-next-line no-unused-vars
-  const basemapOsmTopo = L.tileLayer(
-    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+  const tiles_StadiaStamenTonerLite = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}',
     {
-      attribution:
-        'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
-      maxZoom: 17,
-      name: 'OSM - OpenTopo',
+      maxZoom: 20,
+      name: 'Stadia - Stamen Toner Lite',
       type: 'png',
     }
   );
 
   // eslint-disable-next-line no-unused-vars
-  const basemapOsmMapnik = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  const tiles_OsmMapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
       maxZoom: 19,
-      name: 'OSM - Mapnik',
+      name: 'Open Street Maps - Mapnik',
       type: 'png',
     }
   );
 
-  // OpenStreetMaps Maptiles.
-  providers.push(basemapOsmDefault);
-  providers.push(basemapOsmBw);
-  providers.push(basemapOsmTopo);
-  providers.push(basemapOsmMapnik);
+  providers.push(tiles_CartoDBPositron);
+  providers.push(tiles_StadiaAlidadeSmooth);
+  providers.push(tiles_StadiaStamenTonerLite);
+  providers.push(tiles_OsmMapnik);
 
   Object.keys(providers).forEach((k) => {
     const layer = providers[k];
