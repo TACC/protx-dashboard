3e1ff92b42190bba3b549c2bfa6dc0b0b0577987
John Gentle <taoteg@gmail.com>
2023-10-31 14:19:25 -0500

    Task/wg 169 maptiles service update (#122)
    * Initial commit to switching maptile service for cooks basemap.
    
    * Removed all the STamen based Maptile providers. Reordered the OSM tiles so the cooks map appears as close to the old version as possible by using the black and white maptiles.
    
    * Set the MainMap component to use the first entry in the MapProviders object as the default.
    
    * Updated the tiles provider source array with working tile serviuces from the leaflet providers preview page.
    
    * Ran prettier on the providers file.

--- protx-client/src/components/ProTx/components/maps/MapProviders.jsx
+++ protx-client/src/components/ProTx/components/maps/MapProviders.jsx
@@ -4,104 +4,62 @@ export default function Maptiles() {
   const providers = [];
   const layers = {};
 
-  // Provider: Stamen.
-  // No key required.
   // eslint-disable-next-line no-unused-vars
-  const basemapToner = L.tileLayer(
-    'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}',
+  const tiles_CartoDBPositron = L.tileLayer(
+    'https://abcd.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     {
       attribution:
-        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      subdomains: 'abcd',
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       minZoom: 0,
       maxZoom: 20,
-      ext: 'png',
-      name: 'Stamen - Toner',
-    }
-  );
-
-  const basemapTonerLite = L.tileLayer(
-    'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
-    {
-      attribution:
-        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      subdomains: 'abcd',
-      minZoom: 0,
-      maxZoom: 20,
-      ext: 'png',
-      name: 'Stamen - Toner Light',
-    }
-  );
-
-  // eslint-disable-next-line no-unused-vars
-  const basemapTerrain = L.tileLayer(
-    'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}',
-    {
-      attribution:
-        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      subdomains: 'abcd',
-      minZoom: 0,
-      maxZoom: 18,
-      ext: 'png',
-      name: 'Stamen - Terrain',
-    }
-  );
-
-  // Provider: Open Street Maps
-  // No key required.
-  // eslint-disable-next-line no-unused-vars
-  const basemapOsmDefault = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution:
-        'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      minZoom: 0,
-      maxZoom: 17,
-      name: 'OSM - Default',
+      name: 'CartoDB Positron',
       type: 'png',
     }
   );
 
   // eslint-disable-next-line no-unused-vars
-  const basemapOsmTopo = L.tileLayer(
-    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+  const tiles_StadiaAlidadeSmooth = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
     {
       attribution:
-        'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
-      maxZoom: 17,
-      name: 'OSM - OpenTopo',
+      maxZoom: 20,
+      name: 'Stadia - Alidade Smooth',
       type: 'png',
     }
   );
 
   // eslint-disable-next-line no-unused-vars
-  const basemapOsmMapnik = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  const tiles_StadiaStamenTonerLite = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}',
     {
       attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 0,
+      maxZoom: 20,
+      name: 'Stadia - Stamen Toner Lite',
+      type: 'png',
+    }
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  const tiles_OsmMapnik = L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
       maxZoom: 19,
-      name: 'OSM - Mapnik',
+      name: 'Open Street Maps - Mapnik',
       type: 'png',
     }
   );
 
-  const basemapOsmBw = L.tileLayer(
-    'https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
-    {
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      minZoom: 0,
-      maxZoom: 18,
-      name: 'OSM - Mapnik B&W',
-      type: 'png',
-    }
-  );
-
-  providers.push(basemapOsmBw);
-  providers.push(basemapTonerLite);
+  providers.push(tiles_CartoDBPositron);
+  providers.push(tiles_StadiaAlidadeSmooth);
+  providers.push(tiles_StadiaStamenTonerLite);
+  providers.push(tiles_OsmMapnik);
 
   Object.keys(providers).forEach((k) => {
     const layer = providers[k];
